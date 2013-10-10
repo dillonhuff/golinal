7294e5e2164f3fd8b98f10e5e9799ed1b085e7c0
Dillon Huff <dillonhuff@gmail.com>
2013-10-10 11:43:07 -0700

    now have upper and lower triangular system solvers

--- dense_linear_system.go
+++ dense_linear_system.go
@@ -9,3 +9,32 @@ func MVMul(m *DenseMat, v *DenseVec) *DenseVec {
 	}
 	return prod
 }
+
+/*
+ * Solve dense upper triangular (UT) and lower triangular (LT)
+ * linear systems
+ */
+
+func UTSolve(m *DenseMat, v *DenseVec) *DenseVec {
+	sol := NewVec(v.n)
+	for i := sol.n-1; i >= 0; i-- {
+		sol.ent[i] = v.ent[i]
+		for j := m.cols-1; j > i; j-- {
+			sol.ent[i] -= m.ent[i*m.rows+j]*sol.ent[j]
+		}
+		sol.ent[i] /= m.ent[i*m.rows+i]
+	}
+	return sol
+}
+
+func LTSolve(m *DenseMat, v *DenseVec) *DenseVec {
+	sol := NewVec(v.n)
+	for i := 0; i < m.rows; i++ {
+		sol.ent[i] = v.ent[i]
+		for j := 0; j < i; j++ {
+			sol.ent[i] -= m.ent[i*m.cols+j]*sol.ent[j]
+		}
+		sol.ent[i] /= m.ent[i*m.cols+i]
+	}
+	return sol
+}
--- dense_linear_system_test.go
+++ dense_linear_system_test.go
@@ -18,3 +18,55 @@ func TestMatVecProd(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestSolveUpperTriangular(t *testing.T) {
+	m := New(3, 3)
+	m.Set(0, 0, 1)
+	m.Set(0, 1, 3)
+	m.Set(0, 2, 2)
+	m.Set(1, 1, 6)
+	m.Set(1, 2, 3)
+	m.Set(2, 2, -4)
+	
+	v := NewVec(3)
+	v.Set(0, 24)
+	v.Set(1, 18)
+	v.Set(2, 8)
+	
+	b := UTSolve(m, v)
+	if b.At(2) != -2 {
+		t.FailNow()
+	}
+	if b.At(1) != 4 {
+		t.FailNow()
+	}
+	if b.At(0) != 16 {
+		t.FailNow()
+	}
+}
+
+func TestSolveLowerTriangular(t *testing.T) {
+	m := New(3, 3)
+	m.Set(0, 0, -4)
+	m.Set(1, 0, 3)
+	m.Set(1, 1, 6)
+	m.Set(2, 0, 2)
+	m.Set(2, 1, 3)
+	m.Set(2, 2, 1)
+	
+	v := NewVec(3)
+	v.Set(0, 8)
+	v.Set(1, 18)
+	v.Set(2, 24)
+	
+	b := LTSolve(m, v)
+	if b.At(0) != -2 {
+		t.FailNow()
+	}
+	if b.At(1) != 4 {
+		t.FailNow()
+	}
+	if b.At(2) != 16 {
+		t.FailNow()
+	}
+}
